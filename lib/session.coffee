genieacs/genieacs

commit 68ea4887b1ee23118e0a29b7db1f8e2760775d35

Fix repeating vparam execution
This issue only happens when a device doesn't maintain a single
connection for the entire session.

lib/session.coffee
@@ -1049,15 +1049,20 @@ generateGetVirtualParameterProvisions = (sessionContext, virtualParameterDeclara
   provisions = null
   for declaration in virtualParameterDeclarations
     if declaration[1]
-      provisions ?= []
       currentTimestamps = {}
       currentValues = {}
+      dec = {}
       attrs = sessionContext.deviceData.attributes.get(declaration[0])
+      for k, v of declaration[1]
+        continue if k != 'value' and k != 'writable'
+        if not (v < attrs[k][0])
+          dec[k] = v
       for k, v of attrs
         currentTimestamps[k] = v[0]
         currentValues[k] = v[1]
-      provisions.push([declaration[0][1], declaration[1], {}, currentTimestamps, currentValues])
-      delete declaration[1]
+      if (Object.keys(dec).length)
+        provisions ?= []
+        provisions.push([declaration[0][1], dec, {}, currentTimestamps, currentValues])
 
   return provisions
 

commit 444af5130759fad1b0220a411b51ce8a5c61be28

Fix vparam bug introduced in 68ea488

lib/session.coffee
@@ -1052,10 +1052,10 @@ generateGetVirtualParameterProvisions = (sessionContext, virtualParameterDeclara
       currentTimestamps = {}
       currentValues = {}
       dec = {}
-      attrs = sessionContext.deviceData.attributes.get(declaration[0])
+      attrs = sessionContext.deviceData.attributes.get(declaration[0]) or {}
       for k, v of declaration[1]
         continue if k != 'value' and k != 'writable'
-        if not (v < attrs[k][0])
+        if not attrs[k] or v > attrs[k][0]
           dec[k] = v
       for k, v of attrs
         currentTimestamps[k] = v[0]