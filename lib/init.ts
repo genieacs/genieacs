cd8d264f031e510e64eb7c4e05d48ce68acc821f
Michael Ducharme <mducharme@gmail.com>
2021-03-04 18:39:41 -0800

    Fix typo in default provision

--- lib/init.ts
+++ lib/init.ts
@@ -59,7 +59,7 @@ const INFORM_SCRIPT = `
 // Device ID as user name
 const username = declare("DeviceID.ID", {value: 1}).value[0]
 
-// Password will be fixed a given device because Math.random() is seeded with devcie ID by default.
+// Password will be fixed for a given device because Math.random() is seeded with device ID by default.
 const password = Math.trunc(Math.random() * Number.MAX_SAFE_INTEGER).toString(36);
 
 const informInterval = 300;
