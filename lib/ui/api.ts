7ccdbace705d83114192ff81d8782619d710f6af
Divya Jagadish <divya@genieacs.com>
2022-03-30 11:33:21 -0700

    Authenticate ping API

--- lib/ui/api.ts
+++ lib/ui/api.ts
@@ -660,6 +660,7 @@ router.post("/devices/:id/tags", async (ctx) => {
 });
 
 router.get("/ping/:host", async (ctx) => {
+  if (!ctx.state.user) return void (ctx.status = 401);
   return new Promise<void>((resolve) => {
     ping(ctx.params.host, (err, parsed) => {
       if (parsed) {
