4cb143cc0aa42a7de54964be5eaf32d38b17d1d9
Zaid Humaimidi <zaid@genieacs.com>
2023-11-13 15:18:24 -0800

    Encode event codes containing illegal chars

--- lib/session.ts
+++ lib/session.ts
@@ -60,6 +60,7 @@ import {
 } from "./types";
 import { getRequestOrigin } from "./forwarded";
 import * as logger from "./logger";
+import { encodeTag } from "./util";
 
 const VALID_PARAM_TYPES = new Set([
   "xsd:int",
@@ -216,7 +217,7 @@ export async function inform(
 
   for (const e of rpcReq.event) {
     params.push([
-      Path.parse(`Events.${e.replace(/\s+/g, "_")}`),
+      Path.parse(`Events.${encodeTag(e.replace(/\s+/g, "_"))}`),
       timestamp,
       {
         object: [timestamp, 0],
