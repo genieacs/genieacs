62bedc2d49debe45371155a5cde38746f79a2680
Muhammad Qadri <107682068+bangdrie@users.noreply.github.com>
2024-03-24 01:56:06 +0700

    Update build.ts

--- build/build.ts
+++ build/build.ts
@@ -22,9 +22,12 @@ const fsAsync = {
   mkdir: promisify(fs.mkdir),
 };
 
-const execAsync = promisify(exec);
-
 const MODE = process.env["NODE_ENV"] || "production";
+
+const BUILD_METADATA = new Date()
+  .toISOString()
+  .split(".")[0]
+  .replace(/[^0-9]/g, "");
 
 const INPUT_DIR = process.cwd();
 const OUTPUT_DIR = path.join(INPUT_DIR, "dist");
@@ -137,24 +140,6 @@ function generateSymbol(id: string, svgStr: string): string {
     .map((c) => xmlTostring(c))
     .join("");
   return `<symbol id="icon-${id}" ${viewBox}>${symbolBody}</symbol>`;
-}
-
-async function getBuildMetadata(): Promise<string> {
-  const date = new Date().toISOString().slice(2, 10).replaceAll("-", "");
-
-  const [commit, diff, newFiles] = await Promise.all([
-    execAsync("git rev-parse HEAD"),
-    execAsync("git diff HEAD"),
-    execAsync("git ls-files --others --exclude-standard"),
-  ]).then((res) => res.map((r) => r.stdout.trim()));
-
-  if (!diff && !newFiles) return date + commit.slice(0, 4);
-
-  const hash = createHash("md5");
-  hash.update(commit).update(diff).update(newFiles);
-  for (const file of newFiles.split("\n").filter((f) => f))
-    hash.update(await fsAsync.readFile(file));
-  return date + hash.digest("hex").slice(0, 4);
 }
 
 async function init(): Promise<void> {
