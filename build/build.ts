59cd015a2f047dfafb59ab1286485a949ada1409
Muhammad Qadri <107682068+bangdrie@users.noreply.github.com>
2024-03-24 01:42:56 +0700

    Update build.ts

--- build/build.ts
+++ build/build.ts
@@ -6,6 +6,9 @@ import * as esbuild from "esbuild";
 import { optimize } from "svgo";
 import * as xmlParser from "../lib/xml-parser.ts";
 
+mkdir: promisify(fs.mkdir),
+};
+
 const fsAsync = {
   readdir: promisify(fs.readdir),
   readFile: promisify(fs.readFile),
